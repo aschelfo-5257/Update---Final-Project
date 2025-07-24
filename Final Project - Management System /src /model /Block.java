140cbc55f0f1f57e31193b4a24e0eec6c7aebfd9
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-24 18:07:46 -0400

    Block.java

--- Final Project - Management System /src /model /Block.java
+++ Final Project - Management System /src /model /Block.java
@@ -1,7 +1,8 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class BlockLoader {
+// Block class holds block properties
+public class Block {
     private String name;
     private String texturePath;
     private boolean solid;
@@ -22,14 +23,5 @@ public class BlockLoader {
 
     public boolean isSolid() {
         return solid;
-    }
-  
-    public static ImageView getBlockImageView(Block block) {
-        Image image = new Image(BlockLoader.class.getResourceAsStream(block.getTexturePath()));
-        ImageView imageView = new ImageView(image);
-        return imageView;
-    }
-    public String getName() {
-        return name;
     }
 }
