581675a4aaecc2aca7b97cad0ee565717a001445
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-24 19:20:59 -0400

    Block.java

--- Final Project - Management System /src /model /Block.java
+++ Final Project - Management System /src /model /Block.java
@@ -6,11 +6,37 @@ public class Block {
     private String name;
     private String texturePath;
     private boolean solid;
+    private BlockType type;
+    private int x, y, z;
+    // ... other properties (e.g., durability, light level)
+
+    public Block(BlockType type, int x, int y, int z) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     public Block(String name, String texturePath, boolean solid) {
         this.name = name;
         this.texturePath = texturePath;
         this.solid = solid;
+    }
+
+    public BlockType getType() {
+        return type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
     }
 
     public String getName() {
@@ -24,4 +50,8 @@ public class Block {
     public boolean isSolid() {
         return solid;
     }
+
+    public enum BlockType {
+    GRASS, DIRT, STONE, WOOD, // ... other block types
+}
 }
